ec77da444c082118837dbcd8cbca657ee7954144
yoseplee <joseph38.local@gmail.com>
2018-12-04 18:15:53 +0900

    181204 | 코드와 의미 매핑 완료. 매핑 테이블은 추후 아티팩트로 업로드 예정임. 현재 총 8개가 매핑 되어있음.

--- app/src/main/java/com/aiia/yosep/toucheventex/MainActivity.java
+++ app/src/main/java/com/aiia/yosep/toucheventex/MainActivity.java
@@ -19,6 +19,15 @@ import static android.media.AudioRecord.ERROR;
 Author: Yosep Lee
 HCI 과제로 제작중.
 추후 Vibrator는 Thread로 제어되어야 할 필요가 있을 수 있음.
+매핑 테이블과 입력된 코드와 일치 완료.
+
+ISSUE#1: 입력하다 중간에 멈추게 되면? 타이머로 재고 있다가 refresh를 시켜주는 등 적용이 필요함.
+ISSUE#2: 중간에 입력 취소 하고 싶으면?
+
+추후 의사소통을 위한 인터페이스로 발전할 수 있지 않을까?
+현재는 접촉수화만을 이용하고 있는데,
+입력 방법론 등을 연구하여서 일반인과 의사소통 할 수 있는 인터페이스로 발전 가능하다.
+관건은 이중감각장애자가 직관적으로 입력할 수 있는 약속들을 개발하는 것.
  */
 
 public class MainActivity extends AppCompatActivity {
@@ -30,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
     //진동 관련 변수 제어
     final int MAX_VIBTIME = 3000;
-    final int thresold = 150;
+    final int thresold = 210; //short 미만을 걸러내기 위한 수치.
     final long wait = 150;
 
     //진동을 저장하기 위한 버퍼 변수와 포인터 플래그
@@ -119,10 +128,12 @@ public class MainActivity extends AppCompatActivity {
             normBuf[normBufPos++] = normedInterval;
 
             if(normBufPos > 2) {
+                String toSpeech = convertToText();
+
                 //TTS
                 tts.setPitch(1f);         // 음성 톤 설정
                 tts.setSpeechRate(1f);    // 읽는 속도 설정
-                tts.speak("안녕하세요",TextToSpeech.QUEUE_FLUSH, null);  //tts로 스트링 읽기
+                tts.speak(toSpeech,TextToSpeech.QUEUE_FLUSH, null);  //tts로 스트링 읽기
 
                 //display
                 displayTextView();
@@ -136,9 +147,71 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
+    public String getCodeFromNormBuf() {
+        String result = "";
+        for(int i = 0; i < normBufPos; i++) {
+            switch (normBuf[i]) {
+                case 0:
+                    break;
+                case 1:
+                    result += "1";
+                    break;
+                case 2:
+                    result += "2";
+                    break;
+            }
+        }
+        return result;
+    }
+
+    /*
+        입력된 신호와 그 의미를 매핑한다.
+        매핑 테이블은 구현 아티팩트를 참고할 것.
+        1과 2는 각각 short term vibration과 long term vibration을 의미한다.
+     */
+    public String convertToText() {
+        /*
+        normbuf에 들어있는 터치 값을 텍스트로 변환환다. 111,112, 등 ...
+         */
+        String code = getCodeFromNormBuf();
+        Log.e("**code", code);
+        String result;
+        switch(code) {
+            case "111":
+                result = "이요셉 간병인을 불러주세요.";
+                break;
+            case "112":
+                result = "전 시청각중복 장애인입니다.";
+                break;
+            case "121":
+                result = "죄송합니다.";
+                break;
+            case "211":
+                result = "가까운 병원으로 데려가 주세요.";
+                break;
+            case "122":
+                result = "도와주세요!";
+                break;
+            case "212":
+                result = "감사합니다.";
+                break;
+            case "221":
+                result = "가까운 경찰서로 데려가 주세요.";
+                break;
+            case "222":
+                result = "010 8891 5420 으로 연락 부탁드립니다.";
+                break;
+            default:
+                result = "에러 에러";
+                break;
+        }
+        return result;
+    }
+
     public void displayTextView() {
         //from normalized one.
         String toDisplay = "";
+        String toSpeak = convertToText();
         for(int i = 0; i < normBufPos; i++) {
             switch (normBuf[i]) {
                 case 0:
@@ -151,6 +224,7 @@ public class MainActivity extends AppCompatActivity {
                     break;
             }
         }
+        toDisplay = toDisplay + " \n " + toSpeak;
         textView1.setText(toDisplay);
     }
 
